ec77b01a14e6757a19854967743df1812364a47c
sigmal <1113164237@qq.com>
2025-12-05 15:00:47 +0800

    <feat>(Diary): add key clue

--- Assets/Scripts/UI/Diary/KeyPanel.cs
+++ Assets/Scripts/UI/Diary/KeyPanel.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.UI;
+using Events;
 
 public class KeyPanel : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Tooltip("重要线索图片")]
+    public Image KeyImage;
+
+    void Awake()
     {
-        
+        EventBus.Subscribe<ClueDiscoveredEvent>(OnClueDiscovered);
+        Debug.Log("[KeyPanel.Awake] 已订阅 ClueDiscoveredEvent");
     }
 
-    // Update is called once per frame
-    void Update()
+    void Oestroy()
     {
-        
+        EventBus.Unsubscribe<ClueDiscoveredEvent>(OnClueDiscovered);
+        Debug.Log("[KeyPanel.OnDestroy] 已取消订阅 ClueDiscoveredEvent");
+    }
+
+    void OnClueDiscovered(ClueDiscoveredEvent e)
+    {
+        Debug.Log($"[KeyPanel.OnClueDiscovered] 线索发现事件: {e.clueId}, 重要线索: {e.isKeyClue}");
+        // 如果发现的线索是重要线索，更新 KeyImage
+        if (e.isKeyClue && e.image != null)
+        {
+            KeyImage.sprite = e.image;
+            
+            // 设置宽度为 400pt，高度根据原始比例计算
+            float targetWidth = 400f;
+            float aspectRatio = (float)e.image.texture.height / e.image.texture.width;
+            float targetHeight = targetWidth * aspectRatio;
+            
+            RectTransform rectTransform = KeyImage.GetComponent<RectTransform>();
+            rectTransform.sizeDelta = new Vector2(targetWidth, targetHeight);            
+            Debug.Log($"[KeyPanel.OnClueDiscovered] 重要线索已更新: {e.clueId}, 尺寸设置为 {targetWidth}x{targetHeight}");
+        }
     }
 }
