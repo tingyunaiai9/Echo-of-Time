48909b418cd3f89a48dc62c715a677803db88fb1
sigmal <1113164237@qq.com>
2025-12-18 16:19:50 +0800

    <refactor>(Diary): delete redundant cites

--- Assets/Scripts/UI/Diary/Note.cs
+++ Assets/Scripts/UI/Diary/Note.cs
@@ -9,19 +9,14 @@ public class Note : MonoBehaviour
     public TextMeshProUGUI noteTitleText;
     [Tooltip("便签显示的图片")]
     public Image noteImage;
-    [Header("DetailBar 大图查看")]
-    [SerializeField] Button viewImageButton;       // 右下角按钮
 
     void Awake()
     {
-        viewImageButton = FindFirstObjectByType<Button>();
         // 初始化逻辑
         if (noteTitleText == null)
             noteTitleText = GetComponentInChildren<TextMeshProUGUI>();
         if (noteImage == null)
             noteImage = GetComponentInChildren<Image>();
-        if (viewImageButton != null)
-            viewImageButton.onClick.AddListener(OnClickViewImage);
     }
 
     public void OnClickViewImage()
@@ -31,20 +26,10 @@ public class Note : MonoBehaviour
             Debug.LogWarning($"[{GetType().Name}.OnClickViewImage] noteImage 或其 sprite 未设置");
             return;
         }
-    
-        Debug.Log($"[{GetType().Name}.OnClickViewImage] 点击查看大图");
-    
-        // 查找另一个场景中的 ClueImagePanel（包括非激活对象）
+        
         // 使用 FindObjectsByType 并指定 FindObjectsInactive.Include 来查找未激活对象
         ClueImagePanel clueImagePanel = FindFirstObjectByType<ClueImagePanel>(FindObjectsInactive.Include);
-        if (clueImagePanel == null)
-        {
-            Debug.LogError("[Note.OnClickViewImage] 无法找到 ClueImagePanel");
-            return;
-        }
 
-        
-    
         // 激活 ClueImagePanel
         clueImagePanel.gameObject.SetActive(true);
     
