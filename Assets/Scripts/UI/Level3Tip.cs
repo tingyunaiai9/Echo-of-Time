1389cf6804452821bbae811561c47fdedd7d9e07
sigmal <1113164237@qq.com>
2025-12-23 21:59:50 +0800

    <feat>(tip): add tip content for level3

--- Assets/Scripts/UI/Level3Tip.cs
+++ Assets/Scripts/UI/Level3Tip.cs
@@ -5,11 +5,15 @@ using UnityEngine.UI;
 public class Level3Tip : MonoBehaviour
 {
     public TipManager tipManager; // 引用TipManager脚本
+    public UIManager uiManager; // 引用UIManager脚本
+    public Button closeButton; // 关闭按钮引用
 
     void Start()
     {
         if (tipManager == null) Debug.LogError("Level3Tip: TipManager reference is not set.");
         EventBus.Subscribe<IntroEndEvent>(OnIntroEnd);
+        uiManager = FindFirstObjectByType<UIManager>();
+        closeButton.onClick.AddListener(CloseTipPanel);
     }
 
     private void OnIntroEnd(IntroEndEvent evt)
@@ -19,12 +23,14 @@ public class Level3Tip : MonoBehaviour
         {
             // 启用提示面板
             tipManager.gameObject.SetActive(true);
-            
-            // 查找所有场景中的 UIManager 并调用 SetFrozen(true)
-            UIManager uiManager = FindFirstObjectByType<UIManager>();
             uiManager.SetFrozen(true);
             Debug.Log("[Level3Tip] 已调用 UIManager.SetFrozen(true)");
         }
+    }
+    public void CloseTipPanel()
+    {
+        uiManager.SetFrozen(false);
+        Debug.Log("[Level3Tip] 已调用 UIManager.SetFrozen(false)");
     }
 
     private void OnDestroy()
